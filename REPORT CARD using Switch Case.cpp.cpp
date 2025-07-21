a9c129d53c3a3dc66bc1e61028069c7ff8aa35ec
Shifatpreet <shifatpreetsingh@gmail.com>
2025-07-22 00:58:03 +0530

    Add files via upload

---
+++ REPORT CARD using Switch Case.cpp.cpp
@@ -0,0 +1,92 @@
+//SHIFATPREET SINGH
+//24070123098
+//ENTC B1
+#include<iostream>
+using namespace std;
+int main(){
+    int choice;
+    cout<<"::REPORT CARD::"<<endl;
+    cout<<"WHICH SUBJECT MARKS YOU WANT TO SEE:"<<endl;
+    cout<<"1.PHY"<<endl;
+    cout<<"2.CHEM"<<endl;
+    cout<<"3.MATHS"<<endl;
+    cout<<"4.HINDI"<<endl;
+    cout<<"5.ENGLISH"<<endl;
+    cout<<"enter the choice:"<<endl;
+    cin>>choice;
+    switch(choice){
+        case 1:
+        cout<<"The marks of the phy is: 98";
+        break;
+        case 2:
+        cout<<"The marks of the chem is: 99";
+        break;
+        case 3:
+        cout<<"The marks of the maths is: 97";
+        break;
+        case 4:
+        cout<<"The marks of the hindi is: 96";
+        break;
+        case 5:
+        cout<<"The marks of the english is: 95";
+        break;
+        case 6:
+        cout<<"INVALID CHOICE"<<endl;
+        break;
+    }
+return 0;
+}
+/*::REPORT CARD::
+WHICH SUBJECT MARKS YOU WANT TO SEE:
+1.PHY
+2.CHEM
+3.MATHS
+4.HINDI
+5.ENGLISH
+enter the choice:
+1
+The marks of the phy is: 98
+
+::REPORT CARD::
+WHICH SUBJECT MARKS YOU WANT TO SEE:
+1.PHY
+2.CHEM
+3.MATHS
+4.HINDI
+5.ENGLISH
+enter the choice:
+2
+The marks of the chem is: 99
+
+::REPORT CARD::
+WHICH SUBJECT MARKS YOU WANT TO SEE:
+1.PHY
+2.CHEM
+3.MATHS
+4.HINDI
+5.ENGLISH
+enter the choice:
+3
+The marks of the maths is: 97
+
+::REPORT CARD::
+WHICH SUBJECT MARKS YOU WANT TO SEE:
+1.PHY
+2.CHEM
+3.MATHS
+4.HINDI
+5.ENGLISH
+enter the choice:
+4
+The marks of the hindi is: 96
+
+::REPORT CARD::
+WHICH SUBJECT MARKS YOU WANT TO SEE:
+1.PHY
+2.CHEM
+3.MATHS
+4.HINDI
+5.ENGLISH
+enter the choice:
+5
+The marks of the english is: 95*/
